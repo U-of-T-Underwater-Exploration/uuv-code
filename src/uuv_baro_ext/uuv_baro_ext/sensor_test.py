2c6c964b5e9a0d1b18af1b92b18b96851bb6d1cf
Frank Chen <frank.j.chen2007@gmail.com>
2026-01-31 01:51:13 -0500

    updated sensor_test.py

--- src/uuv_baro_ext/uuv_baro_ext/sensor_test.py
+++ src/uuv_baro_ext/uuv_baro_ext/sensor_test.py
@@ -1,7 +1,11 @@
 from uuv_baro_ext import ms5837
 import time
 
-sensor = ms5837.MS5837_30BA(6) # I2C bus is 6
+try:
+    sensor = ms5837.MS5837_30BA(bus=6) 
+except Exception as e:
+    print(f"Error creating sensor object: {e}")
+    exit(1)
 
 # We must initialize the sensor before reading it
 if not sensor.init():
@@ -23,12 +27,17 @@ print(f"Depth: {freshwaterDepth} m (freshwater)")
 
 print(f"MSL Relative Altitude: {sensor.altitude()} m")
 
+print("Waiting 5 seconds before continuous reading")
 time.sleep(5)
 
 # Output readings
-while True:
-        if sensor.read():
-                print(f"P: {sensor.pressure()}\nC: {sensor.temperature()}")
-        else:
-                print("Sensor read failed!")
-                exit(1)
+print("Starting continuous loop. Press CTRL+C to stop.")
+try:
+    while True:
+            if sensor.read():
+                    print(f"P: {sensor.pressure()}\nC: {sensor.temperature()}")
+            else:
+                    print("Sensor read failed!")
+            time.sleep(0.5)
+except KeyboardInterrupt:
+      print("\nExiting")
