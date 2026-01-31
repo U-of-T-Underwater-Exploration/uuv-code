d546eca3576ca0969fa9ad869bfa25e987e72348
Yangxin Zhou(yxz5029) <yzhou5029@gmail.com>
2026-01-31 14:36:58 -0500

    Joystick-hal update

--- src/uuv_joystick_hal/joystick_hal/input_node.py
+++ src/uuv_joystick_hal/joystick_hal/input_node.py
@@ -10,7 +10,7 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy
 class JoystickHAL(Node):
     def __init__(self):
         super().__init__('joystick_hal')
-
+        self.prev_buttons = []
         qos = QoSProfile(
             depth=10,
             reliability=ReliabilityPolicy.RELIABLE
@@ -24,7 +24,7 @@ class JoystickHAL(Node):
         )
         self.command_pub = self.create_publisher(
             UUVCommand,
-            '/input',
+            '/input/command',
             qos
         )
 
@@ -42,14 +42,32 @@ class JoystickHAL(Node):
             cmd.roll  = 0.0
             cmd.pitch = 0.0
             cmd.mode  = 0
-
+            
+            if not self.prev_buttons:
+                self.prev_buttons = [0] * len(msg.buttons)
+            AXIS_ONLY_BUTTONS = {4, 5}  # Only want the RB/LB to be used as axis not buttons
+            #Corresponding # of button bo be confirmed based on joystick use #4 and #5 for now
+            
             cmd.actions = []
             for i, pressed in enumerate(msg.buttons):
-                if pressed:
-                    a = ActionCommand()
-                    a.action = i
-                    a.state = 1
+                if i in AXIS_ONLY_BUTTONS:
+                    continue
+                prev = self.prev_buttons[i]
+
+                a = ActionCommand()
+                a.action = i
+
+                if pressed and not prev:
+                    a.state = ActionCommand().PRESSED
+                elif not pressed and prev:
+                    a.state = ActionCommand().RELEASED
+                elif pressed and prev:
+                    a.state = ActionCommand().HELD
+                else:
+                    a.state = ActionCommand().INACTIVE
+                if a.state != ActionCommand().INACTIVE:
                     cmd.actions.append(a)
+            self.prev_buttons = list(msg.buttons)
             self.get_logger().info("Joystick message received")
             self.command_pub.publish(cmd)
 
