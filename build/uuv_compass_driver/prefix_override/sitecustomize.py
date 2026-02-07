da4bb868fdf3e5aae7ce6f559e43ded4f74a7777
Jeff Qu <jeffshun123@gmail.com>
2026-02-07 16:51:48 -0500

    file directory change, should match other drivers now

--- build/uuv_compass_driver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jeff/Jeff/UTUX/2025-2026/Template/sensor-compass/uuv-code/install/uuv_compass_driver'
