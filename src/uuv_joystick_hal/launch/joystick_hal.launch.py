615b99d28c836a63a5c9a47c15c4d6000658fcef
Berk Yılmaz <79381262+IDirkI@users.noreply.github.com>
2026-03-03 18:14:12 -0500

    Remove /joy node launch from joystick_hal for integration

--- src/uuv_joystick_hal/launch/joystick_hal.launch.py
+++ src/uuv_joystick_hal/launch/joystick_hal.launch.py
@@ -4,12 +4,6 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package = 'joy',
-            executable = 'joy_node',
-            name = 'joy_node',
-            namespace = 'utux',
-        ),
-        Node(
             package='uuv_joystick_hal',
             executable='joystick_hal',
             name='joystick_hal',
