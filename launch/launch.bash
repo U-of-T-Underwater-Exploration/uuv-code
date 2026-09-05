23c6db6159f1eae35f35ab9527a80bac028dcab9
BoyaZhangNB <harvardboya@gmail.com>
2026-09-04 22:27:07 -0400

    add neutral pub

--- launch/launch.bash
+++ launch/launch.bash
@@ -1,3 +1,3 @@
 colcon build; source install/setup.bash
 ros2 launch launch/ultimate.launch.py &
-SDL_JOYSTICK_DEVICE=/dev/input/js0 ros2 run joy joy_node --ros-args -r __ns:=/utux
+SDL_JOYSTICK_DEVICE=/dev/input/js0 ros2 run joy joy_node --ros-args -r __ns:=/utux -p autorepeat_rate:=20.0 -p deadzone:=0.05
