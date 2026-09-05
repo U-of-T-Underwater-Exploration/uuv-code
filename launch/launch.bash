001ef806ba4e3f04f875c5994c6c2d7de9a96ffd
BoyaZhangNB <harvardboya@gmail.com>
2026-09-04 21:26:46 -0400

    launch two node together

--- launch/launch.bash
+++ launch/launch.bash
@@ -1,3 +1,3 @@
 colcon build; source install/setup.bash
-ros2 launch launch/ultimate.launch.py
+ros2 launch launch/ultimate.launch.py &
 SDL_JOYSTICK_DEVICE=/dev/input/js0 ros2 run joy joy_node --ros-args -r __ns:=/utux
